0604a6287da2077e6e318b80723396e5edb564a2
Hamed Daly <hameddali48@gmail.com>
2025-03-30 12:40:28 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
-import StoreContextProvider from "./context/StoreContext.jsx";
+import StoreContextProvider from "./context/storeContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDom.createRoot(document.getElementById("root")).render(
