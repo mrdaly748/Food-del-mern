81721c44b7129d8220e431413ba695b72c297421
Hamed Daly <hameddali48@gmail.com>
2025-03-30 12:45:02 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Card from "./pages/Card/Card";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
-import Footer from "./components/footer/footer";
+import Footer from "./components/Footer/footer.jsx";
 import LoginpopUp from "./components/LoginPopUp/LoginPopUp"
 import Verify from "./pages/verify/verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
