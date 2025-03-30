9d67526cf1c49f3035db260e926fb0928ba63517
Hamed Daly <hameddali48@gmail.com>
2025-03-30 14:02:22 +0200

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // Placing user order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://food-del-frontend-03bi.onrender.com/";
 
   try {
     const newOrder = new orderModel({
